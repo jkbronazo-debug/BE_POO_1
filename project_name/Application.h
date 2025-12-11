761d11da286c59cff2170dcf345006777f3521a3
EnzoGallego <jkbronazo@gmail.com>
2025-12-11 13:12:01 +0100

    test

--- project_name/Application.h
+++ project_name/Application.h
@@ -1,6 +1,6 @@
 /*********************************************************************
  * @file  Apllication.h
- * @author <mettre l'adresse mail ou nom prenom>
+ * @author Gallego Enzo & Elouan Gilles
  * @brief Fichier header de l'application
  *********************************************************************/
 #ifndef APPLICATION_H_
